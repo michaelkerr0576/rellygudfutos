42d5b1b994e90ea16befc0a7f8f86382c29dc500
michaelkerr0576 <53580213+michaelkerr0576@users.noreply.github.com>
2020-04-17 16:32:58 +0100

    server fix
    server fix

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.VUE_APP_URL,
+  baseURL: process.env.URL || process.env.VUE_APP_URL,
   timeout: 30000,
   // headers: {
   //   Authorization: 'Bearer {token}'
