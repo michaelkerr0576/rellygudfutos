572aeadeea194c493aebde271d237137922a2dfc
michaelkerr0576 <53580213+michaelkerr0576@users.noreply.github.com>
2020-06-22 21:26:39 +0100

    deployment issues again

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const instance = axios.create({
   // TODO - some reason environemental variable is being a bollix
-  // http://localhost:5000 for Dev and process.env.URL for production
+  // http://localhost:5000 for Dev and https://www.rellygudfutos.com for production
   //baseURL: "http://localhost:5000",
-  //baseURL: process.env.URL,
+  //baseURL: "https://www.rellygudfutos.com",
   baseURL: process.env.VUE_APP_URL,
   timeout: 30000,
   // headers: {
