96fb31118ba4ecf4023fc982a380ee121c906ed0
michaelkerr0576 <53580213+michaelkerr0576@users.noreply.github.com>
2020-06-22 21:29:42 +0100

    deployment issues 3

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -4,8 +4,8 @@ export const instance = axios.create({
   // TODO - some reason environemental variable is being a bollix
   // http://localhost:5000 for Dev and https://www.rellygudfutos.com for production
   //baseURL: "http://localhost:5000",
-  //baseURL: "https://www.rellygudfutos.com",
-  baseURL: process.env.VUE_APP_URL,
+  baseURL: "https://www.rellygudfutos.com",
+  //baseURL: process.env.VUE_APP_URL,
   timeout: 30000,
   // headers: {
   //   Authorization: 'Bearer {token}'
